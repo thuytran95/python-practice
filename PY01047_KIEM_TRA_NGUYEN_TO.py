711791fe6cc09286c2dc655f3c99633cc180c6f2
thuytran95 <thuytt0295@gmail.com>
2025-06-29 11:15:11 +0700

    add kiem tra nguyen to problem:

---
+++ PY01047_KIEM_TRA_NGUYEN_TO.py
@@ -0,0 +1,12 @@
+import math
+
+def nguyenTo(n):
+    for i in range(2, math.isqrt(n) + 1):
+        if n % i == 0: return False
+    return n > 1
+
+
+for _ in range(int(input())):
+    s = input().strip()
+    if nguyenTo(int(s[-4:])): print('YES')
+    else: print('NO')
