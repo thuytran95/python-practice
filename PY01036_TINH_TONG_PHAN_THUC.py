8c93edd6824a1a02582e8c7f6502e19e161c541c
thuytran95 <thuytt0295@gmail.com>
2025-06-29 09:01:28 +0700

    refactor tinh tong phan thuc problem

--- PY01036_TINH_TONG_PHAN_THUC.py
+++ PY01036_TINH_TONG_PHAN_THUC.py
@@ -1,11 +1,7 @@
 for _ in range(int(input())):
     n = int(input())
     tong = 0
-    if n % 2 != 0:
-        for i in range(1,n + 1,2):
-            tong += 1/ i
-    else:
-        for i in range(2, n+1, 2):
-            tong += 1/ i
-            
+    start = 1 if n % 2 != 0 else 2   
+    for i in range(start, n + 1 ,2):
+        tong += 1/ i       
     print(f'{tong:.6f}')
